9141fa702d9eaeafc2fe5bf5b36ba574ddeb3f9d
LiziAC05 <lizbeth.aquisse@pucp.edu.pe>
2023-09-30 08:50:27 -0500

    Diseño + ya el boton de Ingresar dirige a la app

--- app/src/main/java/com/example/lab3_iot/MainActivity.java
+++ app/src/main/java/com/example/lab3_iot/MainActivity.java
@@ -14,12 +14,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button = findViewById(R.id.btnIngresar);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, AppActivity.class);
-                startActivity(intent);
-            }
+        button.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, AppActivity.class);
+            startActivity(intent);
         });
     }
 }
