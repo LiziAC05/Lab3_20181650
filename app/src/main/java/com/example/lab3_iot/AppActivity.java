b726fe2f4e61ef4bf0ed022c7250ed15abb5cde6
LiziAC05 <lizbeth.aquisse@pucp.edu.pe>
2023-09-30 18:29:07 -0500

    Mejoras de diseño

--- app/src/main/java/com/example/lab3_iot/AppActivity.java
+++ app/src/main/java/com/example/lab3_iot/AppActivity.java
@@ -6,8 +6,12 @@ import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.example.lab3_iot.databinding.FragmentMagnetometroBinding;
 import com.example.lab3_iot.fragmentos.AcelerometroFragment;
 import com.example.lab3_iot.fragmentos.MagnetometroFragment;
 
